01e56a4ffc76dffe54406efa24c031eadc8ec4ae
disiqui <ditriksy@gmail.com>
2023-03-21 18:34:06 +0200

    Fix theme selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
-document.querySelector('.switch').addEventListener('change', (event) => {
-    if (event.target.nodeName === 'INPUT') {
-        document.documentElement.classList.remove('dark');
-        document.documentElement.classList.add(event.target.value);
-    }
+document.querySelector('#theme-changer').addEventListener('change', (event) => {
+    changeTheme(event.target.checked);
 });
+
+
+document.addEventListener("DOMContentLoaded", (event)  => {
+    var isChecked = document.getElementById("theme-changer").checked;
+    changeTheme(isChecked);
+});
+
+function changeTheme(isChecked){ 
+    if (isChecked) {
+        document.getElementsByTagName("html")[0].setAttribute('class', 'dark');
+    }
+    else {
+        document.getElementsByTagName("html")[0].classList.remove('dark');
+    }
+}
